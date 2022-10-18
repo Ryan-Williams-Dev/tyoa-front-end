eabb382c8acaa97c0379d869f7a6896951c49e04
Ryan Williams <r.d.williams.ca@gmail.com>
2022-10-18 16:36:44 -0700

    added theme and theme providor for material ui

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
+import theme from "./theme/palette";
+import { ThemeProvider } from "@mui/material/styles";
 
 function App() {
   return (
-    <>
-      <Router>
-        <div className="container">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-      </Router>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <Router>
+          <div className="container">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+        </Router>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/palette.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#c4bfe7",
+    },
+    secondary: {
+      main: "#e8d4c0",
+    },
+  },
+});
+
+export default theme;
