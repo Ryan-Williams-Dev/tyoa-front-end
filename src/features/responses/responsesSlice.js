11f0d0b6f9d55a94917c0a849f690cc667a89a55
Ryan Williams <r.d.williams.ca@gmail.com>
2022-11-23 08:39:26 -0800

    fixed lack of name bug in responsesSlice

--- src/features/responses/responsesSlice.js
+++ src/features/responses/responsesSlice.js
@@ -2,7 +2,10 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {};
 
-export const responsesSlice = createSlice({});
+export const responsesSlice = createSlice({
+  name: "responses",
+  initialState,
+});
 
 export const {} = responsesSlice.actions;
 export default responsesSlice.reducer;
